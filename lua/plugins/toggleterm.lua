7a322e5310305d581676a4335e3e2fe6057653c5
hayato-taguchi <taguchiz@seraku.co.jp>
2026-01-30 13:57:17 +0900

    fix: toggletermのキーバインド変更

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,7 +7,7 @@ return {
 
     require("toggleterm").setup({
       size = 15,
-      open_mapping = [[<leader>t]],
+      open_mapping = [[<leader>tt]],
       hide_numbers = true,
       shade_filetypes = {},
       shade_terminals = true,
